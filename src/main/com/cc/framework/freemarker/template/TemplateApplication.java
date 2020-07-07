918b58deaa3b654a898f63cb262486b8c1f314ba
shenjia <547804169@qq.com>
2020-07-07 09:07:58 +0800

    init

---
+++ src/main/com/cc/framework/freemarker/template/TemplateApplication.java
@@ -0,0 +1,17 @@
+package com.cc.framework.freemarker.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author chea0515@163.com
+ */
+@SpringBootApplication
+public class TemplateApplication {
+
+    public static void main(String[] args) {
+        // run app
+        SpringApplication.run(TemplateApplication.class, args);
+    }
+}
---
+++ src/main/com/cc/framework/freemarker/template/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.cc.framework.freemarker.template.controller;
+
+import com.cc.framework.freemarker.template.common.TemplateOutInfo;
+import com.cc.framework.freemarker.template.utils.FreemarkerUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author chea0515@163.com
+ */
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @GetMapping("")
+    public String goIndex(ModelMap model) {
+        //
+        Map<String, Object> fillParam = new HashMap<>();
+        Long tmp = System.currentTimeMillis();
+        fillParam.put("testTxt", "测试模板内容输出:" + tmp);
+        TemplateOutInfo outInfo = FreemarkerUtils.createTemplateFile("outDemo.ftlh", tmp, fillParam);
+        model.put("outInfo", outInfo);
+        return "index";
+    }
+}
